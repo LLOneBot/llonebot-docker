12cc0f2be11820da3df86a8963514de18dade48b
手瓜一十雪 <nanaeonn@outlook.com>
2024-02-28 16:18:07 +0800

    docs: delete

--- fastboot.sh
+++ fastboot.sh
@@ -7,8 +7,8 @@ if command -v docker &> /dev/null; then
 else
     docker_installed=false
 fi
+
 # 安装Docker
-# Version 19.0.0
 if [ "$docker_installed" = true ]; then
     echo "Docker已安装。正在安装脚本..."
     # 在此处添加您的启动命令
@@ -24,7 +24,7 @@ read -p "请配置NoVnc密码: " novnc_password
 
 # 输入启动脚本
 echo "sudo docker run -d --name onebot-docker0 -e VNC_PASSWD=${novnc_password} -p 3000:3000 -p 5900:5900 -p 6081:6081 -p 3001:3001 -v ${PWD}/LiteLoader/:/opt/QQ/LiteLoader/ mlikiowa/llonebot-docker" > start.sh
-echo "安装成功!下次启动执行 sudo sh ./start.sh 即可启动"
+echo "安装成功! 下次启动执行 sudo sh ./start.sh 即可启动"
 
 # 赋予权限
 sudo chmod +x start.sh
